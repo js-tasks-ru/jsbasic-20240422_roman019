986c937d4cd503184769f9f2498cef174b772dd6
roman019 <r.m.kokarev@gmail.com>
2024-04-29 18:58:18 +0300

    Заглавный первый символ

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+
+    newStr = newStr + ((i == 0) ? str[i].toUpperCase() : str[i]);
+  }
+
+  return newStr;
 }
