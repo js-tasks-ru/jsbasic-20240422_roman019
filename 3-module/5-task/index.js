44982d76d0aa79b0d68e38b0b77cd633a849fe8f
roman019 <r.m.kokarev@gmail.com>
2024-05-06 14:45:24 +0300

    MinMax

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+
+  let array = str.split(' ');
+
+  array = array.filter(item => isFinite(item));
+
+  array.sort((a, b) => a - b);
+
+  let result = {
+    min: Number(array[0]),
+    max: Number(array.at(-1)),
+  };
+  return result;
 }
