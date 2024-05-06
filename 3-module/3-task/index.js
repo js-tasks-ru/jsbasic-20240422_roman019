6e6e5c6265c59ea873cd4b183af299dee4d94f72
roman019 <r.m.kokarev@gmail.com>
2024-05-06 13:24:31 +0300

    Camelizer

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,18 @@
 function camelize(str) {
-  // ваш код...
+
+  let array = str.split('-');
+
+  array = array.map((item, i) => {
+    let newItem;
+    if (i > 0 && item.length > 0) {
+      newItem = item[0].toUpperCase() + item.slice(1);
+    } else {
+      newItem = item;
+    };
+    return newItem;
+  });
+
+  array = array.filter(item => item.length > 0);
+
+  return array.join('');
 }
