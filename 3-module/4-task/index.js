e976956a7ca94d6a10c0a684fd00c335443f0e06
roman019 <r.m.kokarev@gmail.com>
2024-05-06 13:55:53 +0300

    Строка с зарплатами

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+
+  let result = '';
+
+  users.forEach(element => {
+    if (element.age <= age) {
+      if (result.length > 0) result += '\n';
+      result += `${element.name}, ${element.balance}`;
+    };
+  });
+
+  return result;
 }
