e645f8bba01fd97b0e85d0a735fa88db711ba039
roman019 <r.m.kokarev@gmail.com>
2024-05-10 17:25:11 +0300

    Список друзей

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+  let ul = document.createElement('ul');
+
+  for (let friend of friends) {
+    let li = document.createElement('li');
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    ul.append(li);
+  };
+
+  return ul;
 }
