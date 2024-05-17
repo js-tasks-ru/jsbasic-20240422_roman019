556b4bfbbe9f50c3531026c48d15ccb18f440a98
roman019 <r.m.kokarev@gmail.com>
2024-05-17 20:17:12 +0300

    Toggle text

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+
+  let button = document.querySelector('.toggle-text-button');
+  let textField = document.getElementById('text');
+
+  button.addEventListener('click', () => textField.hidden = !textField.hidden);
 }
