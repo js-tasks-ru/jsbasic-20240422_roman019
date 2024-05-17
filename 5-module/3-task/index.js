9b294db644f1fb5712cb26f76f2d3ab26e325850
roman019 <r.m.kokarev@gmail.com>
2024-05-17 23:55:33 +0300

    Карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,32 @@
 function initCarousel() {
-  // ваш код...
+
+  let carousel = document.querySelector('.carousel__inner');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
+  let offset = carousel.offsetWidth;
+  let slideIndex = 0;
+  let maxSlideIndex = 3;
+
+  adjustCarouselVisibility();
+
+  arrowRight.addEventListener('click', slideUp);
+  arrowLeft.addEventListener('click', slideDown);
+
+  function adjustCarouselVisibility() {
+    arrowRight.style.display = slideIndex == maxSlideIndex ? 'none' : '';
+    arrowLeft.style.display = slideIndex == 0 ? 'none' : '';
+    carousel.style.transform = `translateX(-${slideIndex * offset}px)`;
+  }
+
+  function slideUp() {
+    if (slideIndex == maxSlideIndex) return;
+    slideIndex++;
+    adjustCarouselVisibility();
+  }
+
+  function slideDown() {
+    if (slideIndex == 0) return;
+    slideIndex--;
+    adjustCarouselVisibility();
+  }
 }
