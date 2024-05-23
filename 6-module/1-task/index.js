f4eb256a2fbc56d6e6fcfe32e0510ff387b0b6a9
roman019 <r.m.kokarev@gmail.com>
2024-05-23 14:05:36 +0300

    Таблица пользователей

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,59 @@
  */
 export default class UserTable {
   constructor(rows) {
+
+    let table = document.createElement('table');
+    let tableHead = document.createElement('thead');
+
+    let headContent = {
+      name: 'Имя',
+      age: 'Возраст',
+      salary: 'Зарплата',
+      city: 'Город',
+    };
+
+    let tHeadRow = createRow(headContent);
+
+    tableHead.append(tHeadRow);
+    table.append(tableHead);
+
+    let tableBody = document.createElement('tbody');
+
+    for (let rowContent of rows) {
+      let row = createRow(rowContent, true);
+      tableBody.append(row);
+    };
+
+    table.append(tableBody);
+    this.elem = table;
+
+    function createRow(content, createButton = false) {
+
+      let row = document.createElement('tr');
+
+      row.addEventListener('click', deleteRow);
+
+      for (let key in content) {
+        let cell = document.createElement('td');
+        cell.innerText = content[key];
+        row.append(cell);
+      };
+
+      let buttonCell = document.createElement('td');
+
+      if (createButton) {
+        let button = document.createElement('button');
+        button.innerText = 'X';
+        buttonCell.append(button);
+      };
+
+      row.append(buttonCell);
+
+      return row;
+    }
+
+    function deleteRow(event) {
+      if (event.target.tagName == "BUTTON") event.currentTarget.remove();
+    }
   }
 }
